f249dd9de757931a2c432898dcde4d72da503a98
IkechiNk <idn7@cornell.edu>
2026-05-26 00:58:40 -0400

    Refactor searchResults and extractDetails to remove redundant headers and improve response handling

--- IkeSource/index.js
+++ IkeSource/index.js
@@ -14,18 +14,6 @@
  */
 async function searchResults(keyword) {
     try {
-        const headers = {
-            'Host': 'reanime.to',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:150.0) Gecko/20100101 Firefox/150.0',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-            'Accept-Language': 'en-US,en;q=0.5',
-            'Alt-Used': 'reanime.to',
-            'Sec-Fetch-Dest': 'document',
-            'Sec-Fetch-Mode': 'navigate',
-            'Sec-Fetch-Site': 'none',
-            'Sec-Fetch-User': '?1',
-            'Upgrade-Insecure-Requests': '1'
-        };
         const search_base = 'https://reanime.to/api/search?limit=36&q='
         const encodedKeyword = encodeURIComponent(keyword);
         const responseText = await soraFetch(`${search_base}${encodedKeyword}`, headers);
@@ -52,20 +40,8 @@ async function searchResults(keyword) {
  */
 async function extractDetails(url) {
     try {
-        const headers = {
-            'Host': 'reanime.to',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:150.0) Gecko/20100101 Firefox/150.0',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-            'Accept-Language': 'en-US,en;q=0.5',
-            'Alt-Used': 'reanime.to',
-            'Sec-Fetch-Dest': 'document',
-            'Sec-Fetch-Mode': 'navigate',
-            'Sec-Fetch-Site': 'none',
-            'Sec-Fetch-User': '?1',
-            'Upgrade-Insecure-Requests': '1'
-        };
         const response = await soraFetch(url, headers);
-        console.log(response)
+        console.log(JSON.parse(response))
         const html = await response.text();
         
         const descriptionRegex = /"description":"([^"]+)"/i;
