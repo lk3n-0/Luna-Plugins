a9b70579b0853ab3988e91356ab552e97e2ba270
IkechiNk <idn7@cornell.edu>
2026-05-26 01:42:08 -0400

    Fix logging in searchResults function to correctly output JSON response

--- IkeSource/index.js
+++ IkeSource/index.js
@@ -8,6 +8,7 @@ async function searchResults(keyword) {
         const search_base = 'https://reanime.to/api/search?limit=36&q='
         const encodedKeyword = encodeURIComponent(keyword);
         const responseText = await soraFetch(`${search_base}${encodedKeyword}`);
+        console.log(responseText.json())
         const data = JSON.parse(responseText);
 
         const transformedResults = data.results.map(anime => ({
